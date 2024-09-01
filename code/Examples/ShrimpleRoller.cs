6f9db3461c963d033712ff9fc750f0074b712a81
yuberee <59583743+yuberee@users.noreply.github.com>
2024-09-01 14:14:50 +0200

    Funny fun fun roller

---
+++ code/Examples/ShrimpleRoller.cs
@@ -0,0 +1,70 @@
+namespace ShrimpleCharacterController;
+
+[Hide]
+public sealed class ShrimpleRoller : Component
+{
+	[RequireComponent]
+	public ShrimpleCharacterController Controller { get; set; }
+
+	public ModelRenderer Renderer { get; set; }
+	public GameObject Camera { get; set; }
+
+	[Property]
+	[Range( 500f, 2000f, 100f )]
+	public float WalkSpeed { get; set; } = 1000f;
+
+	[Property]
+	[Range( 1000f, 5000f, 200f )]
+	public float RunSpeed { get; set; } = 3000f;
+
+	[Property]
+	[Range( 200f, 500f, 20f )]
+	public float JumpStrength { get; set; } = 350f;
+
+	public Angles EyeAngles { get; set; }
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		Renderer = Components.Get<ModelRenderer>( FindMode.EnabledInSelfAndDescendants );
+
+		Camera = new GameObject( true, "Camera" );
+		Camera.SetParent( GameObject );
+		Camera.Components.Create<CameraComponent>();
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
+
+		var wishDirection = Input.AnalogMove * Rotation.FromYaw( EyeAngles.yaw );
+		var isDucking = Input.Down( "Duck" );
+		var isRunning = Input.Down( "Run" );
+		var wishSpeed = isRunning ? RunSpeed : WalkSpeed;
+
+		Controller.WishVelocity = wishDirection * wishSpeed;
+		Controller.Move();
+
+		if ( Input.Pressed( "Jump" ) && Controller.IsOnGround )
+			Controller.Punch( Vector3.Up * JumpStrength );
+	}
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+
+		EyeAngles += Input.AnalogLook;
+		EyeAngles = EyeAngles.WithPitch( MathX.Clamp( EyeAngles.pitch, -10f, 40f ) );
+
+		float pitchRotation = Controller.Velocity.x * Time.Delta;
+		float rollRotation = -Controller.Velocity.y * Time.Delta;
+		var ballPitch = Rotation.FromPitch( pitchRotation );
+		var ballRoll = Rotation.FromRoll( rollRotation );
+		Renderer.Transform.Rotation = ballPitch * ballRoll * Renderer.Transform.Rotation;
+
+		var cameraOffset = Vector3.Up * 70f + Vector3.Backward * 260f;
+		Camera.Transform.Rotation = EyeAngles.ToRotation();
+		Camera.Transform.LocalPosition = cameraOffset * Camera.Transform.Rotation;
+	}
+}
