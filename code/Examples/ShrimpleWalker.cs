92ad14814a88ccbbd1b19c385ebede8b26e395fb
yuberee <59583743+yuberee@users.noreply.github.com>
2024-09-01 13:57:23 +0200

    Walker Example

---
+++ code/Examples/ShrimpleWalker.cs
@@ -0,0 +1,81 @@
+using Sandbox.Citizen;
+
+namespace ShrimpleCharacterController;
+
+[Hide]
+public sealed class ShrimpleWalker : Component
+{
+	[RequireComponent]
+	public ShrimpleCharacterController Controller { get; set; }
+
+	[RequireComponent]
+	public CitizenAnimationHelper AnimationHelper { get; set; }
+	public SkinnedModelRenderer Renderer { get; set; }
+	public GameObject Camera { get; set; }
+
+	[Property]
+	[Range( 50f, 200f, 10f )]
+	public float WalkSpeed { get; set; } = 100f;
+
+	[Property]
+	[Range( 100f, 500f, 20f )]
+	public float RunSpeed { get; set; } = 300f;
+
+	[Property]
+	[Range( 25f, 100f, 5f )]
+	public float DuckSpeed { get; set; } = 50f;
+
+	[Property]
+	[Range( 200f, 500f, 20f )]
+	public float JumpStrength { get; set; } = 350f;
+
+	public Angles EyeAngles { get; set; }
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		Renderer = AnimationHelper.Target;
+		Camera = new GameObject( true, "Camera" );
+		Camera.SetParent( GameObject );
+		Camera.Components.Create<CameraComponent>();
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
+
+		var wishDirection = Input.AnalogMove * Rotation.FromYaw( EyeAngles.yaw );
+		var isDucking = Input.Down( "Duck" );
+		var isRunning = Input.Down( "Run" );
+		var wishSpeed = isDucking ? DuckSpeed :
+			isRunning ? RunSpeed : WalkSpeed;
+
+		Controller.WishVelocity = wishDirection * wishSpeed;
+		Controller.Move();
+
+		if ( Input.Pressed( "Jump" ) && Controller.IsOnGround )
+		{
+			Controller.Punch( Vector3.Up * JumpStrength );
+			AnimationHelper.TriggerJump();
+		}
+
+		AnimationHelper.WithWishVelocity( Controller.WishVelocity );
+		AnimationHelper.WithVelocity( Controller.Velocity );
+		AnimationHelper.DuckLevel = isDucking ? 1f : 0f;
+		AnimationHelper.IsGrounded = Controller.IsOnGround;
+	}
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+
+		EyeAngles += Input.AnalogLook;
+		EyeAngles = EyeAngles.WithPitch( MathX.Clamp( EyeAngles.pitch, -10f, 40f ) );
+		Renderer.Transform.Rotation = Rotation.Slerp( Renderer.Transform.Rotation, Rotation.FromYaw( EyeAngles.yaw ), Time.Delta * 5f );
+
+		var cameraOffset = Vector3.Up * 70f + Vector3.Backward * 220f;
+		Camera.Transform.Rotation = EyeAngles.ToRotation();
+		Camera.Transform.LocalPosition = cameraOffset * Camera.Transform.Rotation;
+	}
+}
